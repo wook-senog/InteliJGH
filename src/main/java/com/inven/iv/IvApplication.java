80e8983953db8a5ea69b9c73e1801eb353deffb1
kkasing <sh_ho@naver.com>
2022-08-01 16:33:50 +0900

    first comit

---
+++ src/main/java/com/inven/iv/IvApplication.java
@@ -0,0 +1,13 @@
+package com.inven.iv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inven/iv/IvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inven.iv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
